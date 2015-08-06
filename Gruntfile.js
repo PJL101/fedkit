79ea6ba5dc6cd579ffead39b9d2651cbfab336b3
Phil Lennon <phil@iampjl.co.uk>
2015-08-06 10:59:09 +0100

    housekeeping

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,23 +349,23 @@ module.exports = function(grunt) {
     // Bump task
     // Use by running: 'grunt bump:patch', 'grunt bump:minor', 'grunt bump:major'
     bump: {
-        options: {
-          files: ['package.json', 'README.md'],
-          updateConfigs: [],
-          commit: true,
-          commitMessage: 'Release v%VERSION%',
-          commitFiles: ['package.json', 'README.md'],
-          createTag: true,
-          tagName: 'v%VERSION%',
-          tagMessage: 'Version %VERSION%',
-          push: true,
-          pushTo: 'origin',
-          gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-          globalReplace: false,
-          prereleaseName: false,
-          regExp: false
-        }
-      },
+      options: {
+        files: ['package.json', 'README.md'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json', 'README.md'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
+    },
   });
 
   // Tasks
